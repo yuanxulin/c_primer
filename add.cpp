c2679280871ddd88ed4f165251766387178b7f57
yuanxulin <2836796746@qq.com>
2020-12-31 21:20:15 +0800

    first

---
+++ add.cpp
@@ -0,0 +1,6 @@
+#include "add.h"
+
+int add(int a, int b){
+    int as = a+b;
+    return a+b;
+}
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "add.h"
+using namespace std;
+int main(){
+    unsigned int a = -2;
+    cout << "hello" << endl;
+    cout << a << endl;
+    cout << add(2, 5) << endl;
+
+    return 0;
+}
